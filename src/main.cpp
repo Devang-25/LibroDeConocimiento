8d3d7c6a22a5f675a9b84a4ff2193078030eda8d
Niel Ponciano <nielgp93@gmail.com>
2019-06-19 21:51:51 +0800

    1. Added functionality in main to accept and display arguments supplied

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+
 int main(int argc, char** argv)
 {
+    std::cout << "\nArguments supplied:";
+    for (int ctr = 0 ; ctr < argc; ctr++)
+    {
+        std::cout << "\n\t" << *(argv+ctr)
+                    << " or " << argv[ctr];
+    }
+
+    std::cout << std::endl;
     return 0;
 }
