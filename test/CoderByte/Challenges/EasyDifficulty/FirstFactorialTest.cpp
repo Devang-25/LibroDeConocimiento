d465c3dc5676a6a68b3220652dda1a21450928e5
Niel Ponciano <nielgp93@gmail.com>
2019-06-28 23:13:15 +0800

    1. Added googletest support 2. Initialized test repository 3. Ported build system to make and run tests as well

---
+++ test/CoderByte/Challenges/EasyDifficulty/FirstFactorialTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+TEST(sample_test_case2, sample_test3)
+{
+    EXPECT_EQ(1, 1);
+}
+
+TEST(sample_test_case2, sample_test4)
+{
+    EXPECT_EQ(11, 11);
+}
