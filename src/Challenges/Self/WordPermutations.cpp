3a7ba36950c60035f4c217f07bd6f8b927b45994
Niel Ponciano <nielgp93@gmail.com>
2019-07-23 10:27:30 +0800

    Added alternative STL algorithm for permutations

--- src/Challenges/Self/WordPermutations.cpp
+++ src/Challenges/Self/WordPermutations.cpp
@@ -36,18 +36,53 @@ void wordPermutate(
     }
 }
 
+void wordPermutate(
+    std::string stringToPermutateSorted,
+    std::set<std::string>& permutations)
+{
+    do
+    {
+        permutations.insert(stringToPermutateSorted);
+    }
+    while (std::next_permutation(stringToPermutateSorted.begin(), stringToPermutateSorted.end()));
+}
+
+template <typename T>
+std::string transformContainerToString(const T& container)
+{
+    std::string result = "";
+    for (const auto& item : container)
+    {
+        result += " " + item;
+    }
+    return result;
+}
+
 std::pair<std::string, unsigned> wordPermutations(
     const std::string& stringToPermutate,
     const unsigned sampleSize)
 {
-    std::set<std::string> permutations;
-    wordPermutate(stringToPermutate, sampleSize, "", permutations);
-
     std::string permutationsString = "";
-    for (const auto& item : permutations)
-    {
-        permutationsString += " " + item;
-    }
+    std::set<std::string> permutations;
+
+    // Algorithm 1: Using std library
+    std::string stringToPermutateSorted = stringToPermutate;
+    std::sort(stringToPermutateSorted.begin(), stringToPermutateSorted.end());
+    wordPermutate(stringToPermutateSorted, permutations);
+    permutationsString = transformContainerToString(permutations);
+    logger.print("Algorithm 1 result - NOTE: This DOES NOT CONSIDER the sample size) : ");
+    logger.print("    Count: ", permutations.size());
+    logger.print("    Permutations: ", permutationsString);
+
+    // Algorithm 2: Using my own function
+    permutationsString.clear();
+    permutations.clear();
+    wordPermutate(stringToPermutate, sampleSize, "", permutations);
+    permutationsString = transformContainerToString(permutations);
+    logger.print("Algorithm 2 result - NOTE: This DOES CONSIDER the sample size) : ");
+    logger.print("    Count: ", permutations.size());
+    logger.print("    Permutations: ", permutationsString);
+
     return {permutationsString, permutations.size()};
 }
 
