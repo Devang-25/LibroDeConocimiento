fd57c925779d2f57568700af764a13bf295b0e94
Niel Ponciano <nielgp93@gmail.com>
2019-07-27 11:31:18 +0800

    Improved unnecessary extra sort in finding next permutation

--- src/Challenges/Self/WordPermutations.cpp
+++ src/Challenges/Self/WordPermutations.cpp
@@ -40,15 +40,17 @@ bool my_next_permutation(std::string& stringToPermutateSorted)
 {
     for (int ctr = stringToPermutateSorted.size() - 1; ctr > 0; --ctr)
     {
+        // Early continue is nice to avoid such nested {{{}}}. But to be clear with the purpose of
+        // this block that finds first occurrence of greater item[ctr] than item[ctr-1] thus is
+        // preferred for increased readability of purpose
         if (stringToPermutateSorted[ctr] > stringToPermutateSorted[ctr-1])
         {
-            std::sort((stringToPermutateSorted.begin() + ctr), stringToPermutateSorted.end());
-            for (unsigned ctr2 = ctr; ctr2 < stringToPermutateSorted.size(); ++ctr2)
+            for (int ctr2 = stringToPermutateSorted.size() - 1; ctr2 >= ctr; --ctr2)
             {
                 if (stringToPermutateSorted[ctr2] > stringToPermutateSorted[ctr-1])
                 {
                     std::swap(stringToPermutateSorted[ctr2], stringToPermutateSorted[ctr-1]);
-                    std::sort((stringToPermutateSorted.begin() + ctr), (stringToPermutateSorted.begin() + ctr2));
+                    std::sort((stringToPermutateSorted.begin() + ctr), stringToPermutateSorted.end());
                     return true;
                 }
             }
